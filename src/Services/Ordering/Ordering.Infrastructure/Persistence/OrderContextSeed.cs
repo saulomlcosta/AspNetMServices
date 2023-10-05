8af52f60ea8dedfacf3d2095660d49ce31463c1c
Saulo Costa <saulomlcosta10@gmail.com>
2023-10-05 08:16:57 -0300

    Implement Ordering.Infrastructure Repositories Persistence, MailService and Config into Ordering.Application

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "smlc", FirstName = "Saulo", LastName = "Costa", EmailAddress = "saulomlcosta10@gmail.com", AddressLine = "Marfagafos", Country = "Brasil", TotalPrice = 350 }
+            };
+        }
+    }
+}
